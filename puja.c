3b06f53d2b18aea7cde2a826fb6d0e429879b473
Asmita Adhikari <asmitadhikari113@gmail.com>
2019-07-23 11:20:42 +0545

    Initial Commit

---
+++ puja.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter a number");
+scanf("%d",&a);
+printf("enter a second number);
+scanf("%d",&b);
+c=a+b;
+printf("the sum=%d",c);
+return 0;
+}
