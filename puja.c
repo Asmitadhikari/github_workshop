5428bd56c30c08dc0cf73ce945cc5bff09db9b82
Asmita Adhikari <asmitadhikari113@gmail.com>
2019-07-23 11:33:27 +0545

    grammar  fixed

--- puja.c
+++ puja.c
@@ -2,9 +2,9 @@
 int main()
 {
 int a,b,c;
-printf("enter a number");
+printf("enter a first number");
 scanf("%d",&a);
-printf("enter a second number);
+printf("enter a second number");
 scanf("%d",&b);
 c=a+b;
 printf("the sum=%d",c);
